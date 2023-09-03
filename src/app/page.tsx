eed77de82e387f5604ec3d1d17c7b98e323efc75
KnK-n <zkskzh718.nk@gmail.com>
2023-09-03 22:24:50 +0900

    delete: css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import UserComponent from "./components/user";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <UserComponent />
     </main>
   );
