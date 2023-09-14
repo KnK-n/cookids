a7628d2d4a1d7361bbac3013488b3694fb4bbe3e
KnK-n <zkskzh718.nk@gmail.com>
2023-09-14 20:08:43 +0900

    add: react-router

---
+++ src/app/api/gets/hooks.ts
@@ -0,0 +1,14 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchRecipes = async () => {
+  const url =
+    "https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=1056336942497159381";
+
+  const res = await fetch(url);
+  console.log(res);
+  return res.json();
+};
+
+const recipesResult = useQuery({ queryKey: ["recipes"], queryFn: fetchRecipes });
+console.log(recipesResult.data);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Header } from "./components/layout/header";
+import { Recipe } from "./components/recipe";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      {/* ↓test */}
+      <Recipe />
     </main>
   );
 }
